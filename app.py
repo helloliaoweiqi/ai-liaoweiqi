fb5f8159022c092f872f7b7d0f251720f1e633ee
helloliaoweiqi <liaoweiqi_0325@qq.com>
2026-09-11 21:45:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
+#-*-coding: utf-8-*-
 import os
 import re
 import json
 import uuid
 
 import streamlit as st
-from utils import llm, prompts, storage
+form utils import llm,prompts,storage
+
+st.write("ok")
 
 st.set_page_config(
     page_title="智学伴侣‑AI个性化学习助手",
